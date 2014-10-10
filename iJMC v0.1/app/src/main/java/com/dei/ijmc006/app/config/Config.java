ce1ba71923574f19cee0b1f6ddf53f910129ff81
omiplekevin <omiple.kevin@gmail.com>
2014-10-10 21:17:21 +0800

    config file update

--- iJMC v0.1/app/src/main/java/com/dei/ijmc006/app/config/Config.java
+++ iJMC v0.1/app/src/main/java/com/dei/ijmc006/app/config/Config.java
@@ -34,7 +34,9 @@ public class Config {
 
     public static String BASE_URL = "http://192.168.56.1/iJMC-WebApp/public";
 
-    public static String JSON_URL = "http://192.168.1.127/iJMC-WebApp/public/jsonlisting";
+//    public static String JSON_URL = "http://192.168.43.74/iJMC-WebApp/public/jsonlisting";
+
+    public static String JSON_URL = "http://192.168.43.74/ijmc3/public/jsonlisting";
 
     public static String CONTENT_JSON = "contentlist.json";
 
@@ -57,10 +59,10 @@ public class Config {
     public String jmcProfile;
 
     public String LOGIN = "LOGIN";
+
     /*
     SHARED PREFERENCE FIELDS
      */
-
     public static String SHA_USR_ID = "USR_ID";
 
     public static String SHA_USR_FNAME = "USR_FNAME";
@@ -73,22 +75,44 @@ public class Config {
 
     //    CONTENT TABLE TAGS
     public static final String TAG_CONTENT_ID = "id";
+
     public static final String TAG_CONTENT_TYPE = "content_type";
+
     public static final String TAG_CONTENT_BODY = "content_body";
 
     //    FACULTY TABLE TAGS
     public static final String TAG_FACULTY_ID = "fc_idn";
+
     public static final String TAG_FACULTY_FNAME = "fc_name";
+
     public static final String TAG_FACULTY_MNAME = "fc_mname";
+
     public static final String TAG_FACULTY_LNAME = "fc_lname";
+
     public static final String TAG_FACULTY_SFX = "fc_suffix";
+
     public static final String TAG_FACULTY_GENDER = "fc_gender";
+
     public static final String TAG_FACULTY_IMAGE = "image_path";
+
     public static final String TAG_FACULTY_DEPT = "dept_id";
+
     public static final String TAG_FACULTY_POSITION = "pos";
 
     //    DEPARTMENT TABLE TAGS
     public static final String TAG_DEPT_ID = "id";
+
     public static final String TAG_DEPT_TITLE = "dept_title";
+
     public static final String TAG_DEPT_DESC = "dept_desc";
+
+    public static ArrayList<String> getJsonUrls()
+    {
+        ArrayList<String> jsonUrls = new ArrayList<String>();
+        jsonUrls.add(JSON_URL + "/" + CONTENT_JSON);
+        jsonUrls.add(JSON_URL + "/" + DEPARTMENT_JSON);
+        jsonUrls.add(JSON_URL + "/" + STUDENT_JSON);
+        return jsonUrls;
+    }
 }
+
